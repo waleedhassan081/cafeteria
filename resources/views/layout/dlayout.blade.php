99cf872e48bb859076697b852dddce883ae0f437
waleedhassan081 <waleedhassan081@gmail.com>
2019-07-11 12:36:49 +0400

    initial dashboard setup

---
+++ resources/views/layout/dlayout.blade.php
@@ -0,0 +1,130 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cafeteria System</title>
+        <!-- Bootstrap -->
+        <link href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+        <!-- NProgress -->
+        <link href="{{asset('vendors/nprogress/nprogress.css')}}" rel="stylesheet">
+
+        <!-- Custom Theme Style -->
+        <link href="{{asset('build/css/custom.min.css')}}" rel="stylesheet">
+    </head>
+    <body class="nav-md">
+        <div class="container body">
+          <div class="main_container">
+            <div class="col-md-3 left_col">
+              <div class="left_col scroll-view">
+                <div class="navbar nav_title" style="border: 0;">
+                  <a href="#" class="site_title"><i class="fa fa-paw"></i> <span>Cafeteria System</span></a>
+                </div>
+    
+                <div class="clearfix"></div>
+    
+                <!-- menu profile quick info -->
+                <div class="profile clearfix">
+                  <div class="profile_pic">
+                  <img src="{{asset('images/img.jpg')}}" alt="..." class="img-circle profile_img">
+                  </div>
+                  <div class="profile_info">
+                    <span>Welcome,</span>
+                    <h2>John Doe</h2>
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+                <!-- /menu profile quick info -->
+    
+                <br />
+    
+                <!-- sidebar menu -->
+                <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                  <div class="menu_section">
+                    <h3>General</h3>
+                    <ul class="nav side-menu">
+                      <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                          <li><a href="#">Dashboard</a></li>
+                          <li><a href="#">Dashboard2</a></li>
+                          <li><a href="#">Dashboard3</a></li>
+                        </ul>
+                      </li>
+                     
+                    </ul>
+                  </div>
+    
+                </div>
+                <!-- /sidebar menu -->
+    
+                <!-- /menu footer buttons -->
+                <div class="sidebar-footer hidden-small">
+                  <a data-toggle="tooltip" data-placement="top" title="Settings">
+                    <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                  </a>
+                  <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                    <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+                  </a>
+                  <a data-toggle="tooltip" data-placement="top" title="Lock">
+                    <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+                  </a>
+                  <a data-toggle="tooltip" data-placement="top" title="Logout" href="#">
+                    <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+                  </a>
+                </div>
+                <!-- /menu footer buttons -->
+              </div>
+            </div>
+    
+            <!-- top navigation -->
+            <div class="top_nav">
+              <div class="nav_menu">
+                <nav>
+                  <div class="nav toggle">
+                    <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                  </div>
+    
+                  <ul class="nav navbar-nav navbar-right">
+                    <li class="">
+                      <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <img src="{{asset('images/img.jpg')}}" alt="">John Doe
+                        <span class=" fa fa-angle-down"></span>
+                      </a>
+                      <ul class="dropdown-menu dropdown-usermenu pull-right">
+                        <li><a href="#"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <!-- /top navigation -->
+    
+          @yield('content')
+            <!-- footer content -->
+            <footer>
+              <div class="pull-right">
+                copyright (©) 2019 All Rights Reserved
+              </div>
+              <div class="clearfix"></div>
+            </footer>
+            <!-- /footer content -->
+          </div>
+        </div>
+    
+        <!-- jQuery -->
+        <script src="{{asset('vendors/jquery/dist/jquery.min.js')}}"></script>
+        <!-- Bootstrap -->
+        <script src="{{asset('vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        <!-- FastClick -->
+        <script src="{{asset('vendors/fastclick/lib/fastclick.js')}}"></script>
+        <!-- NProgress -->
+        <script src="{{asset('vendors/nprogress/nprogress.js')}}"></script>
+        
+        <!-- Custom Theme Scripts -->
+        <script src="{{asset('build/js/custom.min.js')}}"></script>
+      </body>
+    </html>
+    
